a63a421734a1e5801b20681c9ea40232fa102d18
anagpur1 <abhishek_nagpure@mcafee.com>
2018-03-05 23:22:53 +0530

    Adding the Login form .

--- auth/src/main/java/com/nfactorial/demo/DemoApplication.java
+++ auth/src/main/java/com/nfactorial/demo/DemoApplication.java
@@ -1,15 +1,23 @@
 package com.nfactorial.demo;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
@@ -25,6 +33,12 @@ public class DemoApplication {
     return model;
   }
 
+  @RequestMapping("/user")
+  @ResponseBody
+  public Principal user(Principal user) {
+    return user;
+  }
+  
   @GetMapping(value = "/{path:[^\\.]*}")
   public String redirect() {
     return "forward:/";
@@ -34,6 +48,27 @@ public class DemoApplication {
     SpringApplication.run(DemoApplication.class, args);
   }
 }
+
+
+@Configuration
+ @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER) 
+class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+        .httpBasic()
+      .and()
+        .authorizeRequests()
+          .antMatchers("/index.html", "/", "/home", "/login").permitAll()
+          .anyRequest()
+          .authenticated()
+          .and()
+          .csrf()
+          .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+          
+    }
+  }
+
 
 @Component
 @ConfigurationProperties("demo")
